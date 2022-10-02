ec4c6e1882fcd72acb649e769c2dc9ba4e2a47a9
Phumipat Chaiprasertsud <phumipat.c@outlook.com>
2022-10-02 10:03:02 +0700

    Add Closest pair algorithm

--- closest_pair.cpp
+++ closest_pair.cpp
@@ -1,13 +1,5 @@
 #include<bits/stdc++.h>
-#define all(x) begin(x),end(x)
-#define sz(x) (int)(x).size()
-#define MOD (LL )(1e9+7)
 using namespace std;
-using LL = long long;
-using PII = pair<int ,int >;
-using PLL = pair<long long ,long long >;
-const int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
-const int dir8[2][8] = {{-1,-1,-1,0,1,1,1,0},{-1,0,1,1,-1,0,1,-1}};
 
 const int N = 100010;
 struct Point {
